8fc0342ca9a6fcb20e06505a008a82cb7f27c5d4
= <=>
2016-03-29 20:32:29 +0800

    f

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # nameko_service
 # nameko_service
+
+
+start the service:
+nameko run service:ServiceB
+
+
+in another termial,test the service
+nameko shell
+n.dispatch_event("service_a", "event_type", "hello2")
--- service.py
+++ service.py
@@ -1,49 +1,22 @@
-from nameko.events import event_handler
-import mandrill
-from jsonschema import validate, exceptions
+from nameko.events import EventDispatcher, event_handler
+from nameko.rpc import rpc
+
+class ServiceA(object):
+    """ Event dispatching service. """
+    name = "service_a"
+
+    dispatch = EventDispatcher()
+
+    @rpc
+    def dispatching_method(self, payload):
+        self.dispatch("event_type", payload)
 
 
-class Mailer(object):
-    """ This is a main service class accepting messages payloads from payments service.
+class ServiceB(object):
+    """ Event listening service. """
+    name = "service_b"
 
-        TODO:
-        - Remove hard dependency on Mandrill and allow for swapping for different implementations.
-            Potentially use concept of Nameko Dependencies.
-        - Move Madrill API Key to environment variable
-    """
-    name = "mailer"
-
-    mailer_provider = mandrill.Mandrill("8eZMbUkC5HFrN79k1JwHBg")
-
-
-    @event_handler("payments", "payment_received")
+    @event_handler("service_a", "event_type")
     def handle_event(self, payload):
+        print("service b received:", payload)
 
-        """
-        Args:
-            payload (object): all necessary data for sending email client
-        """
-
-        print "Payload received", payload
-
-        results = {
-            "success": False,
-            "errors": []
-        }
-
-        if not validate_payload(payload):
-            results["success"] = False
-            results["errors"].append("Invalid Payload")
-            return results
-
-        try:
-            message_text = construct_message_text(payload)
-            message = construct_message(message_text, payload)
-            self.mailer_provider.messages.send(message=message)
-
-            results["success"] = True
-            return results
-
-        except mandrill.Error, e:
-            results["success"] = False
-            results["errors"].append("A mandrill error occurred: %s - %s" % (e.__class__, e))
